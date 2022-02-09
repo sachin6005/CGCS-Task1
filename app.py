c7526480619a4be1f30ecf21bd611ed719d275ac
sachin6005 <85541761+sachin6005@users.noreply.github.com>
2022-02-09 12:42:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from healthcheck import HealthCheck
 app = Flask(__name__)
 health = HealthCheck()
 
+
 @app.route("/")
 def root_path():
     return("Welcome to my world")
@@ -14,10 +15,13 @@ def root_path():
 def name_path():
     return("My Name is SACHIN")
 
+
 @app.route("/health")
 def health_check():
     return("healthy")
 
+
 app.add_url_rule('/healthcheck', 'healthcheck', view_func=lambda: health.run())
+
 
 app.run(port=8080, host="0.0.0.0")
